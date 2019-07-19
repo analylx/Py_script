625329837acd4814fa7e7d586686bce6ec373b14
Sheng Fang <a322494@ecitele.com>
2019-07-19 13:36:44 +0800

    2

--- combind_sinple.py
+++ combind_sinple.py
@@ -1,7 +1,7 @@
 from itertools import combinations, permutations
 
-ne_name = ['1800 1t', '1800 2t', '1250', '1300', '1200i', '1050i']
-version = ['7.5', '7.6', '8.0']
+ne_name = ['1800 2t', '1300', '1200i', '1050i']
+version = ['7.5', '7.6']
 feature = ["IPv6 interface", "IPv6 ping&TR", "IP BFD"]
 
 if __name__ == "__main__":
@@ -13,6 +13,7 @@ if __name__ == "__main__":
                 if i == "1050i" and j == "8.0":
                     continue
                 temp = (i, j, k)
+                print(temp)
                 result.append(temp)
     print("Total combines: {0} \n".format(len(result)),result)
     #print(list(permutations([1, 2, 3], 2)))
